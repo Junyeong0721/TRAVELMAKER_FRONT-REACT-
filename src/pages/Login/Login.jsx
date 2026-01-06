0b1b69be8431392aa81da6a818337e56d66da084
JJY <Junyeong0721>
2026-01-06 16:19:21 +0900

    api 연결

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,7 +1,64 @@
+import axios from 'axios';
 import React from 'react';
+import {login} from '../api/login/loginService';
+import {boardList} from '../api/게시판테스트/boardService';
 import './Login.css';
 
-const Login = () => {
+const Login = () => { 
+
+  function 로그인() {
+    const id = document.getElementById("id");
+    const pw = document.getElementById("pw");
+
+    const obj = {
+      userId: id.value,
+      userPw: pw.value
+    }
+    login(obj) // req, res
+      .then(res => {
+        console.log(res);
+
+        if (res.status !== 200) {
+
+          console.log('error');
+          return;
+        }
+        const restoken = res.data.data;
+
+        if (restoken === null){
+          alert("아이디와 패스워드를 확인해주세요");
+          return;
+        }
+        console.log('토큰 발급 확인 : ' + restoken);
+        document.cookie = "token=" + restoken + "; path=/; max-age=86400";
+        document.cookie = "userId=" + id.value + "; path=/; max-age=86400";
+      })
+  }
+  function getCookie(name) {//테스트용
+    const value = document.cookie
+      .split('; ')
+      .find(row => row.startsWith(name + '='));
+    return value ? value.split('=')[1] : null;
+
+
+  }
+  function 쿠키확인() {//테스트용
+    const token = getCookie('token');
+    console.log('토큰확인 : ' + token);
+
+  }
+  function api호출() {//테스트용
+      const token = getCookie('token');
+      const userId = getCookie('userId');
+
+      boardList()
+        .then(res => {
+          console.log(res);
+        }).catch(err => {
+          console.log("error");
+        });
+    }
+
   return (
     <div className="login-page">
       <div className="ticket-container">
@@ -47,13 +104,13 @@ const Login = () => {
           
           <div className="input-group">
             <label>아이디</label>
-            <input type="text" placeholder="아이디를 입력하세요" />
+            <input type="text" placeholder="아이디를 입력하세요" id = "id"/>
           </div>
 
           <div className="input-group">
             <label>비밀번호</label>
             <div className="password-wrapper">
-              <input type="password" placeholder="••••••••" />
+              <input type="password" placeholder="••••••••" id = "pw"/>
               <span className="eye-icon">👁</span>
             </div>
           </div>
@@ -65,8 +122,9 @@ const Login = () => {
             <a href="#find" className="find-link">비밀번호 찾기</a>
           </div>
 
-          <button className="login-submit-btn">로그인</button>
-
+          <button className="login-submit-btn" onClick={로그인}>로그인</button>
+          <button className="testbutton" onClick={쿠키확인}>쿠키확인</button>
+          <button className="apitestbutton" onClick={api호출}>API호출</button>
           {/* 이 부분이 박스 내부 최하단에 고정됩니다 */}
           <div className="signup-prompt">
             <span>계정이 없으신가요?</span>
